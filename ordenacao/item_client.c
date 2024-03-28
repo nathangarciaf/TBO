b12fd02e111f263b761b2a4cd5293adce8137afc
nathangarciaf <nathangarfreitas@gmail.com>
2024-03-28 09:58:30 -0300

    ord

---
+++ ordenacao/item_client.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "item.h"
+
+extern void sort(Item *a, int lo, int hi);
+
+int main(){
+    int n = 0;
+    scanf("%d",&n);
+
+    Item *array = (Item*)calloc(n,sizeof(Item));
+    for(int i = 0; i < n; i++){
+        scanf("%d", &array[i]);
+    }
+
+    sort(array, 0, n-1);
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    free(array);
+    return 0;
+}
