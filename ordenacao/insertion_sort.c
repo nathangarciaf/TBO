5a8890e6bb497339c416b96029a3305245737785
nathangarciaf <nathangarfreitas@gmail.com>
2024-03-28 19:18:22 -0300

    ord

--- ordenacao/insertion_sort.c
+++ ordenacao/insertion_sort.c
@@ -5,12 +5,13 @@
 extern void sort(Item *a, int lo, int hi){
     for(int i = lo; i < hi; i++){
         int valor = a[i+1];
-        int pos_valor = a[i+1];
+        int pos_valor = i+1;
         for(int j = i; j > lo; j--){
             if(less(valor, a[j])){
-                
+                a[j+1] = a[j];
             }
             else{
+                a[j] = valor;
                 break;
             }
         }
